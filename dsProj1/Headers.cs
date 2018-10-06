728149d6a760da358ea514208fc3af022ca944af
Tirth Patel <tirth.patel111196@gmail.com>
2018-10-06 16:18:50 -0500

    Added a Header class

---
+++ dsProj1/Headers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace dsProj1
+{
+    public enum Headers : byte
+    {
+        Queue,
+        Start,
+        Stop,
+        Pause,
+        Chunk
+    }
+}
