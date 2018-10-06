71a664da10a98c2bba1842d97e04e3323a4a1257
Tirth Patel <tirth.patel111196@gmail.com>
2018-10-06 16:12:01 -0500

    Add project files.

---
+++ dsProj1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dsProj1
+{
+    static class Program
+    {
+        /// The main entry point for the application.
+
+        //For ID generation
+        public static Random Rand;
+
+        [STAThread]
+        static void Main()
+        {
+            Rand = new Random();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
